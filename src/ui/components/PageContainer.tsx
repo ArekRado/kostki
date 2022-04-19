6e542e178c0007b4a14ffb2c0e7f5c3df2df6af1
Arek Rado <arekrrr@gmail.com>
2022-04-19 19:46:05 +0200

    fix page container size

--- src/ui/components/PageContainer.tsx
+++ src/ui/components/PageContainer.tsx
@@ -4,10 +4,7 @@ export const PageContainer = styled('div', {
   display: 'grid',
   margin: '0 auto',
   padding: '0.5rem',
-  '@bp2': {
-    maxWidth: '768px',
-  },
   '@bp3': {
-    maxWidth: '1080px',
+    maxWidth: '1920px',
   },
 })
